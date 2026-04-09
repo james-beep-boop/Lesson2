ab7bd1d01a19c2700cbec6511274ed7c0fa96dbc
James BeepBoop <priority2@protonmail.ch>
2026-04-09 07:43:13 -0700

    Buttons on the guide page

--- resources/views/filament/app/pages/guide.blade.php
+++ resources/views/filament/app/pages/guide.blade.php
@@ -1,19 +1,21 @@
 <x-filament-panels::page>
     <div class="max-w-2xl">
         {{-- Language toggle --}}
-        <div class="mb-6 flex items-center gap-3">
-            <x-filament::button
-                wire:click="switchLanguage('en')"
-                :color="$language === 'en' ? 'primary' : 'gray'"
-            >
-                English
-            </x-filament::button>
-            <x-filament::button
-                wire:click="switchLanguage('sw')"
-                :color="$language === 'sw' ? 'primary' : 'gray'"
-            >
-                Swahili
-            </x-filament::button>
+        <div class="mb-6 rounded-xl border border-gray-200 bg-gray-50 p-4 dark:border-gray-700 dark:bg-gray-800/50">
+            <div class="flex flex-wrap gap-3">
+                <x-filament::button
+                    wire:click="switchLanguage('en')"
+                    :color="$language === 'en' ? 'primary' : 'gray'"
+                >
+                    English
+                </x-filament::button>
+                <x-filament::button
+                    wire:click="switchLanguage('sw')"
+                    :color="$language === 'sw' ? 'primary' : 'gray'"
+                >
+                    Swahili
+                </x-filament::button>
+            </div>
         </div>
 
         {{-- Guide sections --}}
