4a480c02cdc546c25351a1ec26929a56f8cb2329
James BeepBoop <63834492+james-beep-boop@users.noreply.github.com>
2026-04-23 06:19:27 -0700

    Revert "Download the manual"
    This reverts commit 2779e386a553fde516bf1afad08ad33b851d7f73.

--- app/Http/Controllers/GuideManualController.php
+++ app/Http/Controllers/GuideManualController.php
@@ -11,14 +11,7 @@ class GuideManualController extends Controller
         abort_unless(in_array($lang, ['en', 'sw']), 404);
         abort_unless(auth()->check(), 403);
 
-        // Get cached PDF (generated once), or generate if missing
-        $pdfPath = $manuals->pdfPath($lang);
-        if (! file_exists($pdfPath)) {
-            $pdf = $manuals->renderIllustratedPdf($lang);
-            file_put_contents($pdfPath, $pdf);
-        }
-
-        return response(file_get_contents($pdfPath))
+        return response($manuals->renderPdf($lang))
             ->header('Content-Type', 'application/pdf')
             ->header('Content-Disposition', 'attachment; filename="'.$manuals->pdfFilename($lang).'"');
     }
